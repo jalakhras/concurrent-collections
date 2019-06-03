fb939f2ff0e0a7f5734c39b0e91c914de4863587
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-04 02:07:46 +0300

    Create basic Dictionary oprations

--- BasicDictionaryOps/Program.cs
+++ BasicDictionaryOps/Program.cs
@@ -10,12 +10,26 @@ namespace BasicDictionaryOps
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            var stock = new Dictionary<string, int>()
+            {
+                {"jDays", 4},
+                {"technologyhour", 3}
+            };
+            Console.WriteLine(string.Format("No. of shirts in stock = {0}", stock.Count));
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            stock.Add("JMA", 6);
+            stock["buddhistgeeks"] = 5;
+
+            stock["JMA"] = 7; // up from 6 - we just bought one			
+            Console.WriteLine(string.Format("\r\nstock[JMA] = {0}", stock["JMA"]));
+
+            stock.Remove("jDays");
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (var keyValPair in stock)
+            {
+                Console.WriteLine("{0}: {1}", keyValPair.Key, keyValPair.Value);
+            }
         }
     }
 }
