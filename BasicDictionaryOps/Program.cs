51614fcfbf89e25d8459a1e1771127ccccb5cf6b
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-06 01:40:15 +0300

    Converting to ConcurrentDictionary

--- BasicDictionaryOps/Program.cs
+++ BasicDictionaryOps/Program.cs
@@ -1,29 +1,44 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BasicDictionaryOps
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            var stock = new Dictionary<string, int>()
-            {
-                {"jDays", 4},
-                {"technologyhour", 3}
-            };
+            var stock = new ConcurrentDictionary<string, int>();
+            stock.TryAdd("jDays", 4);
+            stock.TryAdd("technologyhour", 3);
             Console.WriteLine(string.Format("No. of shirts in stock = {0}", stock.Count));
 
-            stock.Add("JMA", 6);
+            var success =  stock.TryAdd("JMA", 6);
+            Console.WriteLine("Added success ?  "+ success);
+
+            success= stock.TryAdd("JMA", 6);
+            Console.WriteLine("Added success ?  " + success);
+
             stock["buddhistgeeks"] = 5;
 
-            stock["JMA"] = 7; // up from 6 - we just bought one			
-            Console.WriteLine(string.Format("\r\nstock[JMA] = {0}", stock["JMA"]));
+            //stock["JMA"] = 7; // up from 6 - we just bought one	
+            //success = stock.TryUpdate("JMA", 7, 6);
+            //Console.WriteLine("JMA = {0} , did Update Work? {1} ", stock["JMA"],success);
 
-            stock.Remove("jDays");
+            //success = stock.TryUpdate("JMA", 8, 6);
+            //Console.WriteLine("JMA = {0} , did Update Work? {1} ", stock["JMA"], success);
+
+            // stock["JMA"]++ ; 
+            int psStock = stock.AddOrUpdate("JMA", 1, (key, oldValue) => oldValue + 1);
+            Console.WriteLine("New value is "+ psStock);
+
+            Console.WriteLine(string.Format("stock[JMA] = {0}", stock.GetOrAdd("JMA",0)));
+
+
+            Console.WriteLine(string.Format("\r\nstock[JMA] = {0}", stock["JMA"]));
+            success = stock.TryRemove("jDays", out int jDaysValue);
+            Console.WriteLine("Value removed was : " + jDaysValue);
 
             Console.WriteLine("\r\nEnumerating:");
             foreach (var keyValPair in stock)
