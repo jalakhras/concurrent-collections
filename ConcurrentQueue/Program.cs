63c4d6f2f4dbcc846707be21029b3e689448f0ad
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-15 19:29:51 +0300

    Concurrent queue sampel

--- ConcurrentQueue/Program.cs
+++ ConcurrentQueue/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,20 +11,28 @@ namespace ConcurrentQueue
     {
         static void Main(string[] args)
         {
-            var shirts = new Queue<string>();
+            var shirts = new ConcurrentQueue<string>();
             shirts.Enqueue("Pluralsight");
             shirts.Enqueue("WordPress");
             shirts.Enqueue("Code School");
 
             Console.WriteLine("After enqueuing, count = " + shirts.Count.ToString());
 
-            string item1 = shirts.Dequeue();
-            Console.WriteLine("\r\nRemoving " + item1);
+            string item1; //= shirts.Dequeue();
+            bool success = shirts.TryDequeue(out item1);
+            if (success)
+                Console.WriteLine("\r\nRemoving " + item1);
+            else
+                Console.WriteLine("queue was empty");
 
-            string item2 = shirts.Peek();
-            Console.WriteLine("Peeking   " + item2);
+            string item2; //= shirts.Peek();
+            success = shirts.TryPeek(out item2);
+            if (success)
+                Console.WriteLine("Peeking   " + item2);
+            else
+                Console.WriteLine("queue was empty");
 
-            Console.WriteLine("\r\nEnumerating:");
+            Console.WriteLine("\r\nEnumerating:");s
             foreach (string item in shirts)
                 Console.WriteLine(item);
 
