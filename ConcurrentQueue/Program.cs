a33ab5959f45be6b50547f36a6e35d04fd311922
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-15 19:43:23 +0300

    Demo Concurrent Bag sampel

--- ConcurrentQueue/Program.cs
+++ ConcurrentQueue/Program.cs
@@ -14,6 +14,8 @@ namespace ConcurrentQueue
             DemoConcurrentQueue();
             Console.WriteLine("****************");
             DemoConcurrentStack();
+            Console.WriteLine("****************");
+            DemoConcurrentBag();
         }
 
         private static void DemoConcurrentQueue()
@@ -74,6 +76,36 @@ namespace ConcurrentQueue
 
             Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
         }
+        private static void DemoConcurrentBag()
+        {
+            var shirts = new ConcurrentBag<string>();
+            shirts.Add("Pluralsight");
+            shirts.Add("WordPress");
+            shirts.Add("Code School");
+
+            Console.WriteLine("After enqueuing, count = " + shirts.Count.ToString());
+
+            string item1; //= shirts.Dequeue();
+            bool success = shirts.TryTake(out item1);
+            if (success)
+                Console.WriteLine("\r\nRemoving " + item1);
+            else
+                Console.WriteLine("queue was empty");
+
+            string item2; //= shirts.Peek();
+            success = shirts.TryPeek(out item2);
+            if (success)
+                Console.WriteLine("Peeking   " + item2);
+            else
+                Console.WriteLine("queue was empty");
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (string item in shirts)
+                Console.WriteLine(item);
+
+            Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
+        }
+
 
 
     }
