8944e54eef75b0bb1b0f9374dc700ae159c34e2c
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-06-15 19:36:45 +0300

    Concurrent Stack sampel

--- ConcurrentQueue/Program.cs
+++ ConcurrentQueue/Program.cs
@@ -11,6 +11,13 @@ namespace ConcurrentQueue
     {
         static void Main(string[] args)
         {
+            DemoConcurrentQueue();
+            Console.WriteLine("****************");
+            DemoConcurrentStack();
+        }
+
+        private static void DemoConcurrentQueue()
+        {
             var shirts = new ConcurrentQueue<string>();
             shirts.Enqueue("Pluralsight");
             shirts.Enqueue("WordPress");
@@ -32,11 +39,42 @@ namespace ConcurrentQueue
             else
                 Console.WriteLine("queue was empty");
 
-            Console.WriteLine("\r\nEnumerating:");s
+            Console.WriteLine("\r\nEnumerating:");
             foreach (string item in shirts)
                 Console.WriteLine(item);
 
             Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
         }
+        private static void DemoConcurrentStack()
+        {
+            var shirts = new ConcurrentStack<string>();
+            shirts.Push("Pluralsight");
+            shirts.Push("WordPress");
+            shirts.Push("Code School");
+
+            Console.WriteLine("After Push, count = " + shirts.Count.ToString());
+
+            string item1; //= shirts.Dequeue();
+            bool success = shirts.TryPop(out item1);
+            if (success)
+                Console.WriteLine("\r\nRemoving " + item1);
+            else
+                Console.WriteLine("queue was empty");
+
+            string item2; //= shirts.Peek();
+            success = shirts.TryPeek(out item2);
+            if (success)
+                Console.WriteLine("Peeking   " + item2);
+            else
+                Console.WriteLine("queue was empty");
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (string item in shirts)
+                Console.WriteLine(item);
+
+            Console.WriteLine("\r\nAfter enumerating, count = " + shirts.Count.ToString());
+        }
+
+
     }
 }
